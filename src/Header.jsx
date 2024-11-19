c5561134eb15690b88645d70123f022730ae8c56
ImoEazy <imieadams@gmail.com>
2024-11-19 11:46:16 +0200

    changed getMemes function

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react"
-import Image from "../image/troll-face.png"
 
 export default function Header() {
     return (
-    <header className="header">
-        <img src={Image} className="header--image" />
-        <h1 className="header--title">Meme Generator</h1>
-        <h4 className="header--project">React Course</h4>
-    </header>
+        <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
     )
 }
